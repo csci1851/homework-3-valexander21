56a7b6df2cc68ff8e10a05de25d3c50f7091f601
Vanessa Alexander <vanessa_alexander@brown.edu>
2026-03-04 09:15:25 -0500

    filled out

--- src/model.py
+++ src/model.py
@@ -6,24 +6,40 @@ def svm_classifier(kernel: str = "linear", C: float = 1.0, degree: int = 3, gamm
     """
     TODO: Return a scikit-learn SVC model with the specified parameters.
     """
-    pass
+    svc = SVC(C=C, kernel=kernel, degree=degree, gamma=gamma)
+    return svc
 
 
 def svm_regressor(kernel: str = "linear", C: float = 1.0, degree: int = 3, gamma: str = "scale"):
     """
     TODO: Return a scikit-learn SVR model with the specified parameters.
     """
-    pass
+    svr = SVR(kernel=kernel, C=C, degree=degree, gamma=gamma)
+    return svr
 
 def evaluate_classifier(model, X_test, y_test):
     """
     TODO: Compute and return accuracy, precision, recall, and F1 score
     """
-    pass
 
+    y_pred = model.predict(X_test)
+
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    return accuracy, precision, recall, f1
 
 def evaluate_regressor(model, X_test, y_test):
     """
     TODO: Compute and return MAE, RMSE, and R2
     """
-    pass
+
+    y_pred = model.predict(X_test)
+
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    return mae, mse, r2
